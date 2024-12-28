02d5ebf287e8baadf1f1f123664b1ad96769a04a
Henry W <150013588+Henry8r8w@users.noreply.github.com>
2024-12-27 18:59:56 -0800

    Update comparator_decomposed.sv

--- Digital Design and Hardware/CSE 369 - UofWa/comparator_decomposed.sv
+++ Digital Design and Hardware/CSE 369 - UofWa/comparator_decomposed.sv
@@ -4,7 +4,7 @@ module comparator_decomposed(input  logic [2:0] A, B,
   // subtraction result (intermediate)
   logic [2:0] sub; // 0-2 bits -> 3 bits
 
-  assign sub = A - B; // == 1 (negtaive), == 0 (positive)
+  assign sub = A - B; // == 1 (negative result), == 0 (positive result)
 
   // 1 denotes true and 0 denotes false for the conditions
   assign is_eq = ~(sub[0] | sub[1] | sub[2]);
