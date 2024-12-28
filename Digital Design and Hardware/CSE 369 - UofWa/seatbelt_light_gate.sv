623f7fb42fa8aa68307323306372821b7b436a1d
Henry W <150013588+Henry8r8w@users.noreply.github.com>
2024-12-27 19:11:55 -0800

    Update seatbelt_light_gate.sv

--- Digital Design and Hardware/CSE 369 - UofWa/seatbelt_light_gate.sv
+++ Digital Design and Hardware/CSE 369 - UofWa/seatbelt_light_gate.sv
@@ -1,13 +1,45 @@
+// Block Diagram (SBL, DBI, PBI, P)
+// DBI -> NOT()_1 =: NOT_DBI -> AND_3() =: A
+// PBI -> NOT()_2 =: NOT_PBI
+// P -> AND_3() =: A
+// A -> OR_4, NOT_DBI -> OR_4 =: SBL
+//
+
+// Gates Approach
 module seatbelt_light_gate(input  logic DBI, PBI, P,
                            output logic SBL);
-  // Intermediate signals: initialization, wait to be assgined
-  logic NOT_DBI, NOT_PBI, A;
+  
+  logic NOT_DBI, NOT_PBI, A; // Intermediate signals, wait to be assigned
 
-  
-  // Individual signal assignments: gate level
-  not gate1(NOT_DBI, DBI);     // ~DBI
-  not gate2(NOT_PBI, PBI);     // ~PBI
-  and gate3(A, P, NOT_PBI);    // P & ~PBI
-  or  gate4(SBL, A, NOT_DBI);  // A | NOT_DBI
-  
-endmodule  // seatbelt_light_gate
+  not gate1(NOT_DBI, DBI);    
+  not gate2(NOT_PBI, PBI);     
+  and gate3(A, P, NOT_PBI);    
+  or  gate4(SBL, A, NOT_DBI);  
+
+endmodule
+
+// Intermediate Signals Assignment Approach
+module seatbelt_light (input logic DBI, PBI, P,
+                           output logic SBL);
+        output logic SBL;
+        input logic  DBI, PBI, P;
+        logic NOT_DBI, NOT_PBI, A, B; // Intermediate signals, wait to be assigned
+
+        assign NOT_DBI = ~DBI;
+        assign NOT_PBI = ~PBI;
+        assign A = NOT_PBI & P;
+        assign B = A | NOT_DBI;
+
+        assign SBL = B;
+
+endmodule
+
+// Single Assignment Approach
+module seatbelt_light (input logic DBI, PBI, P,
+                           output logic SBL);
+        output logic SBL;
+        input logic  DBI, PBI, P;
+
+        assign SBL = (~PBI & P)| ~DBI;
+
+endmodule
